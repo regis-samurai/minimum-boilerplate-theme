8ccdb7ff1028bbf8f8078ecb12c16ecec8b2aaf0
Dracenco <andreidracenco@yahoo.com>
2020-11-13 23:37:18 +0200

    1

--- react/WishlistPage.js
+++ react/WishlistPage.js
@@ -18,7 +18,6 @@ const WishlistPage = (props) => {
           namespace="my-custom-page"
           title="wishlist"
           backButton={backButton}>
-        {({ handleError }) => children}
     <ExtensionPoint id="list-context" />
       </ContentWrapper>
   return null
