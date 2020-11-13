84f114ee17b5d19b6ad219fab8786949384183b1
Dracenco <andreidracenco@yahoo.com>
2020-11-13 23:21:34 +0200

    back button

--- react/WishlistPage.js
+++ react/WishlistPage.js
@@ -1,8 +1,26 @@
 import React from 'react'
 import { ExtensionPoint } from "vtex.render-runtime"
+import {
+  ContentWrapper,
+  BaseLoading,
+  GenericError,
+} from "vtex.my-account-commons"
+
+const backButton = {
+  title: "indietro",
+  titleId: "indietro-wishlist",
+  path: "/account"
+};
 
 const WishlistPage = (props) => {
-  if(props.location.pathname == "/wishlist") return <ExtensionPoint id="list-context" />
+
+  if(props.location.pathname == "/wishlist") return <ContentWrapper
+          namespace="my-custom-page"
+          title="wishlist"
+          backButton={backButton}>
+        {({ handleError }) => children}
+    <ExtensionPoint id="list-context" />
+      </ContentWrapper>
   return null
 }
 
